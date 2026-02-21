e869dad1b333d2227382abf6b8b249fd701e2bc2
MNohava1987 <mnohava1987@gmail.com>
2026-02-21 13:45:35 -0600

    adjusting role id name

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ locals {
 
 # Resolve the Space Admin role ID for permission granting
 data "spacelift_role" "space_admin" {
-  role_id = "Space Admin"
+  slug = "space-admin"
 }
 
 # --- 1) CONSTITUTIONAL POLICIES (ENVIRONMENT-SPECIFIC) ---
