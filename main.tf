e6b49e6a8e8c8347c5692d497f33895d5fd73809
MNohava1987 <mnohava1987@gmail.com>
2026-02-21 14:09:18 -0600

    fixing data block for space admin

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ locals {
 
 # Resolve the Space Admin role ID for permission granting
 data "spacelift_role" "space_admin" {
-  role_id = "space-admin"
+  slug = "Space admin"
 }
 
 # --- 1) CONSTITUTIONAL POLICIES (ENVIRONMENT-SPECIFIC) ---
